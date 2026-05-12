92059152c1bbabc988fc1ec7a455c8dc5159d959
James Taylor <james.taylor9109@protonmail.com>
2026-05-12 14:05:46 -0700

    Setting up population of the array with random points between 0 and 1 and counting which points are within the circle

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,37 @@
+#include <cmath>
 #include <iostream>
 #include <string>
+#include <random>
 
 #include "lib.hpp"
 
+static auto getDist(double x1, double y1, double x2, double y2) -> double {
+  return sqrt(pow(x1 - x2, 2) - pow(y1-y2,2));
+}
+
 static auto approximate_pi() -> double {
+  std::random_device rand;
+  std::mt19937 gen(rand());
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+  const int length = 100;
+  double points[length][2];
+
+  for(auto & point : points) {
+    point[0] = dist(gen);
+    point[1] = dist(gen);
+  }
+
+  int pointsIn = 0;
+
+  for (auto & point : points) {
+    if(0.5 <= getDist(point[0],point[1],0.5,0.5)){
+      pointsIn++;
+    }
+  }
+  // Area of square is A = s^2
+  // Area of circle is A = Pi*r^2
+
   return 0.0;
 }
 
