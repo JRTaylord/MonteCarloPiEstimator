ec755f812899696ef85bb9b527f760d65a0d27f1
James Taylor <james.taylor9109@protonmail.com>
2026-05-12 17:14:12 -0700

    Formatting

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,17 @@
 #include <cmath>
 #include <iostream>
-#include <string>
 #include <random>
+#include <string>
 
 #include "lib.hpp"
 
-static auto getDist(double x1, double y1, double x2, double y2) -> double {
-  return sqrt(pow(x1 - x2, 2) + pow(y1-y2,2));
+static auto getDist(double x1, double y1, double x2, double y2) -> double
+{
+  return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
-static auto approximate_pi() -> double {
+static auto approximate_pi() -> double
+{
   std::random_device rand;
   std::mt19937 gen(rand());
   std::uniform_real_distribution<double> dist(0.0, 1.0);
@@ -18,26 +20,34 @@ static auto approximate_pi() -> double {
 
   int pointsIn = 0;
 
-  for (int i=0; i<pointsTotal; i++) {
-    if(0.5 >= getDist(dist(gen),dist(gen),0.5,0.5)){
+  for (int i = 0; i < pointsTotal; i++) {
+    if (0.5 >= getDist(dist(gen), dist(gen), 0.5, 0.5)) {
       pointsIn++;
     }
   }
   // Area of square is A = s^2
   // Area of circle is A = Pi*r^2
   //
-  // The percentage of points in the circle should be pointsIn/pointsTotal = (Pi*r^2)/(s^2)
+  // The percentage of points in the circle should be pointsIn/pointsTotal =
+  // (Pi*r^2)/(s^2)
+  //
   // Then I need to pull Pi out from this:
+  //
   // pointsTotal/pointsIn = (s^2)/(Pi*r^2)
+  //
   // Pi * pointsTotal/pointsIn = (s^2)/(r^2)
+  //
   // Pi = (s^2)/(r^2) * pointsIn/pointsTotal
+  //
   // Pi = (r*2)^2)/(r^2) * pointsIn/pointsTotal
+  //
   // Pi = (r*r*4)/(r*r) * pointsIn/pointsTotal
+  //
   // Pi = 4 * pointsIn/pointsTotal
   std::cout << "pointsIn: " << pointsIn << '\n';
   std::cout << "pointsTotal: " << pointsTotal << '\n';
 
-  return 4.0 * static_cast<double>(pointsIn)/static_cast<double>(pointsTotal);
+  return 4.0 * static_cast<double>(pointsIn) / static_cast<double>(pointsTotal);
 }
 
 auto main() -> int
